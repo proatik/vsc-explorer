adbec07ce2965532f93ec80a180db5f4f8db2dca
Atik Ullah Khan <atikullah.murad@gmail.com>
2025-04-11 17:50:45 +0600

    feat: integrate main-context-provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-const App = () => {
+import { MainContextProvider } from "./contexts/MainContext";
+
+function App() {
   return (
     <div className="w-screen h-screen p-4 sm:p-8 md:p-12">
-      <div className="flex flex-row w-full h-full border-2 rounded-md border-slate-600 bg-[#0b0c25]"></div>
+      <div className="flex flex-row w-full h-full border-2 rounded-md border-slate-600 bg-[#0b0c25]">
+        <MainContextProvider>
+          <span></span>
+        </MainContextProvider>
+      </div>
     </div>
   );
-};
+}
 
 export default App;
