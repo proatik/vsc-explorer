1a008652ce06a6f10426dbf4d91115113aee9771
Atik Ullah Khan <atikullah.murad@gmail.com>
2025-04-11 17:52:00 +0600

    feat: integrate sidebar and explorer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { MainContextProvider } from "./contexts/MainContext";
 
+import Sidebar from "./components/Sidebar";
+import Explorer from "./components/Explorer";
+
 function App() {
   return (
     <div className="w-screen h-screen p-4 sm:p-8 md:p-12">
       <div className="flex flex-row w-full h-full border-2 rounded-md border-slate-600 bg-[#0b0c25]">
         <MainContextProvider>
-          <span></span>
+          <Sidebar />
+          <Explorer />
         </MainContextProvider>
       </div>
     </div>
