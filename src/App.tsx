186380f32ccbdadabf03fee20023eee9e7641164
Atik Ullah Khan <atikullah.murad@gmail.com>
2025-04-11 16:45:21 +0600

    feat: initialize react project with tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-screen h-screen p-4 sm:p-8 md:p-12">
+      <div className="flex flex-row w-full h-full border-2 rounded-md border-slate-600 bg-[#0b0c25]"></div>
+    </div>
+  );
+};
+
+export default App;
